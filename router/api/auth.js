fd9260b46106fa1538d9627b6a0843aaf36683f5
Masud Rana <MasudRana917775@gmail.com>
2023-10-24 20:58:40 +0600

    add validation feature

--- router/api/auth.js
+++ router/api/auth.js
@@ -1,4 +1,6 @@
 const AuthController = require('../../app/controllers/api/auth-controller')
+const { validate } = require('../../app/middleware/validate')
+const ValidationRules = require('../../app/validators/auth')
 
 module.exports = {
     group: {
@@ -8,11 +10,13 @@ module.exports = {
         {
             method: 'post',
             path: '/login',
+            middleware: [ValidationRules.login, validate],
             handler: AuthController.login
         },
         {
             method: 'post',
             path: '/register',
+            middleware: [ValidationRules.register, validate],
             handler: AuthController.register
         }
     ]
