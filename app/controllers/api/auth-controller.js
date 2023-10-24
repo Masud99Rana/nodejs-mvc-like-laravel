db9f0b3e9543f896fa477f8ed39a52b17ff0eeee
Masud Rana <MasudRana917775@gmail.com>
2023-10-24 20:12:22 +0600

    add auth feature, custom exceptions

---
+++ app/controllers/api/auth-controller.js
@@ -0,0 +1,38 @@
+const { User } = require('../../models')
+const InvalidCredentialException = require('../../exceptions/invalid-credential-exception')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const { appKey, tokenExpiresIn } = require('../../../config/app')
+
+class AuthController {
+
+    async login(req, res) {
+        const { email, password } = req.body
+
+        const user = await User.findOne({
+            where: {
+                email
+            }
+        })
+
+        if (!user)
+            throw new InvalidCredentialException()
+
+        if (!await bcrypt.compare(password, user.password))
+            throw new InvalidCredentialException()
+
+        const payload = { id: user.id, email: user.email, firstName: user.firstName, lastName: user.lastName }
+        // const key = require('crypto').randomBytes(64).toString('hex')
+
+        const accessToken = jwt.sign(payload, appKey, { expiresIn: tokenExpiresIn })
+
+        res.send({ user, ...{ accessToken } })
+    }
+
+    async register(req, res) {
+        res.send('Register')
+    }
+
+}
+
+module.exports = new AuthController()
